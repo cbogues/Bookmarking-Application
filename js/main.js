3a2c5dff1379f109c7b08b44ad0b1cd220bd0818
cbogues <YOUR-EMAIL-ADDRESS>
2017-03-01 20:39:00 -0800

    errors on focusin focusout dom events

--- js/main.js
+++ js/main.js
@@ -1,23 +1,42 @@
-(function(){
+(function() {
 
 	const body = document.body;
 	const input = document.querySelector('input[type=next]');
-	const overlay = document.querySelector('overlay');
+	const overlay = document.querySelector('.overlay');
 
-	function showFloater(){
+	function showFloater() {
 		body.classList.add('show-floater');
 	}
 
 
 	function closeFloater() {
-		if(body.classList.contains('show-floater')) {
+		if (body.classList.contains('show-floater')) {
 		body.classList.remove('show-floater');
 		}
 	}
 
+ 	input.addEventListener('focus', showFloater);
+ 	console.log(input);
+    input.addEventListener('blur', closeFloater);
+    console.log(input);
+    overlay.addEventListener('click', closeFloater);
 
-input.addEventListener('focusin', showFloater);
-input.addEventListener('focusout', closeFloater);
-overlay.addEventListener('click', closeFloater);
+
+		// ======= for Showing bookmarks
+
+	const bookmarksList = document.querySelector('.bookmarks-list');
+	const bookmarkForm = document.querySelector('.bookmark-form');
+	const bookmarkInput = bookmarkForm.querySelector('input[type=text]');
+
+	function createBookmark(e) {
+	e.preventDefault();
+
+	console.log('processing the form');
+	}
+
+
+
+	bookmarkForm.addEventListener('submit', createBookmark);
+
 
 })();
