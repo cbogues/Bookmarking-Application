a54686eb8d419110158bbf7089ff2ba4008729c2
cbogues <YOUR-EMAIL-ADDRESS>
2017-03-04 12:57:28 -0800

    added ability to store to LocalStorage

--- js/main.js
+++ js/main.js
@@ -22,31 +22,88 @@
     overlay.addEventListener('click', closeFloater);
 
 
-		// ======= for Showing bookmarks
+
+
+		// ==============
 
 	const bookmarksList = document.querySelector('.bookmarks-list');
 	const bookmarkForm = document.querySelector('.bookmark-form');
 	const bookmarkInput = bookmarkForm.querySelector('input[type=text]');
+	const bookmarks = JSON.parse(localStorage.getItem('bookmaks')) || [];
+
+	// console.table(bookmarks);
+
+	fillBookmarksList(bookmarks);
+
 
 	function createBookmark(e) {
 	e.preventDefault();
 
-	// grab info out of the input and create element of the bookmark
+
+	// add a new bookmark to the bookmarks
 	const title  = bookmarkInput.value;
-	const bookmark = document.createElement('a');
-	bookmark.className = 'bookmark';
-	bookmark.innerText = title;
-	bookmark.href = '#';
-	bookmark.target = '_blank';
-	bookmarksList.appendChild(bookmark);
+	const bookmark = {
+		title: title
+	};
+
+
+	bookmarks.push(bookmark);
+	fillBookmarksList(bookmarks);
+	storeBookmarks(bookmarks);
+	bookmarkForm.reset();
+
+
+	console.table(bookmarks);
+
+	// save that list to local storage
+
+	// grab info out of the input and create element of the bookmark
+	// const title  = bookmarkInput.value;
+	// const bookmark = document.createElement('a');
+	// bookmark.className = 'bookmark';
+	// bookmark.innerText = title;
+	// bookmark.href = '#';
+	// bookmark.target = '_blank';
+	// bookmarksList.appendChild(bookmark);
 
 	// clear bookmark form
 	
-	bookmarkForm.reset();
 
 	}
 
+	// take a parameter 'bookmarks' if there are no bookmarks
+	// default to blank array. For each of these bookmarks add it to the list
+	function fillBookmarksList(bookmark = []) {
+		const bookmarksHtml = bookmarks.map((bookmark) => {
+		return	` 
+			<a href="#" class="bookmark">
+			${bookmark.title}
+			</a>
+			`;
+		}).join(' ');
 
+	bookmarksList.innerHTML = bookmarksHtml;
+
+
+// 
+// for loop below vs map above
+// 
+	// 	let bookmarksHtml = '';
+	// 	// way to loop over every bookmark
+	// for (let i = 0; i < bookmarks.length; i++){
+	// 	bookmarksHtml += ` 
+	// 		<a href="#" class="bookmark">
+	// 		${bookmarks[i].title}
+	// 		</a>
+	// 		`;
+	// 	}
+		// console.log(bookmarksHtml);
+	}
+
+
+	function storeBookmarks(bookmarks = []){
+		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	}
 
 	bookmarkForm.addEventListener('submit', createBookmark);
 
